c435f2ca2dcac1c713600728a9feded55fd90cde
Andrew Zacharia <andrew.zacharia@carterdigital.com>
2020-12-09 15:55:48 +1100

    craft cms demo: Layouts and blocks updates, Tailwind styling

--- src/scripts/swiper.js
+++ src/scripts/swiper.js
@@ -7,7 +7,15 @@ import 'swiper/swiper-bundle.css';
 /**
  * Swiper carousel from https://swiperjs.com/get-started/
  */
-var swiper = new Swiper('.swiper-container', {
+var cardCarousel = new Swiper('.c-card-carousel', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+});
+
+var heroSwiper = new Swiper('.hero-container', {
   spaceBetween: 30,
   pagination: {
     el: '.swiper-pagination',
